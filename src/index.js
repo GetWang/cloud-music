a7b3eedd466a8f18f8fc3b241cf916b44c3a5e66
GetWang <wang_dresimap@qq.com>
2021-08-27 19:49:59 +0800

    style(src/common/scss): add reset and base style

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import './common/scss/index.scss';
 import App from './App';
 import { store } from './app/store';
 import * as serviceWorker from './serviceWorker';
