37d729d0f20f4aa5c546af452ec3aa51711255ff
GetWang <wang_dresimap@qq.com>
2021-08-28 20:34:26 +0800

    feat(src/App.js): add route of page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Switch, Route, Redirect } from 'react-router-dom';
 
 import './App.scss';
 
 function App() {
   return (
-    <div id="app">app</div>
+    <div id="app">
+      <BrowserRouter>
+        <NavLink to="/home" activeClassName="active">/home</NavLink>
+        <NavLink to="/songListCategory" activeClassName="active">/songListCategory</NavLink>
+        <NavLink to="/songList" activeClassName="active">/songList</NavLink>
+        <Switch>
+          <Redirect exact from="/" to="/home"></Redirect>
+          <Route path="/home">home</Route>
+          <Route path="/songListCategory">songListCategory</Route>
+          <Route path="/songList">songList</Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
