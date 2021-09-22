93aac930d24b98e1f2f8539b058db62cf9a2e2ce
GetWang <wang_dresimap@qq.com>
2021-09-22 16:08:59 +0800

    feat(serverApi): add api on server

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,10 +1,19 @@
 import React from "react";
+import axios from 'axios'
 
 import "./home.scss";
 
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:4000/personalized').then(res => {
+      console.log('res', res.data)
+    }).catch(e => {
+      console.log('e', e)
+    })
   }
 
   render() {
