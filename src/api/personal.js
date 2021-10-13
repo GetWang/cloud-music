58cbc675a2e9a0087dc182516d1ce0ca526ac1c2
GetWang <wang_dresimap@qq.com>
2021-10-13 22:22:10 +0800

    feat(mock): add mock data for gh-pages

--- src/api/personal.js
+++ src/api/personal.js
@@ -2,18 +2,32 @@ import axios from "axios";
 
 import { API_HOST } from "./common";
 
+import DailyRecomRes from "../mock/DailyRecomRes.json";
+import PersonalFMRes from "../mock/PersonalFMRes.json";
+import RecomSongListsRes from "../mock/RecomSongListsRes.json";
+
 export function getDailyRecommendSongs() {
   const url = API_HOST + "/personalized/newsong";
-  return axios.get(url).then((res) => {
-    return res.data;
-  });
+  return axios
+    .get(url)
+    .then((res) => {
+      return res.data;
+    })
+    .catch(() => {
+      return DailyRecomRes;
+    });
 }
 
 export function getPersonalizedFmSongs() {
   const url = API_HOST + "/personal_fm";
-  return axios.get(url).then((res) => {
-    return res.data;
-  });
+  return axios
+    .get(url)
+    .then((res) => {
+      return res.data;
+    })
+    .catch(() => {
+      return PersonalFMRes;
+    });
 }
 
 export function getRecommendSongLists(params = {}) {
@@ -24,5 +38,8 @@ export function getRecommendSongLists(params = {}) {
     })
     .then((res) => {
       return res.data;
+    })
+    .catch(() => {
+      return RecomSongListsRes;
     });
 }
